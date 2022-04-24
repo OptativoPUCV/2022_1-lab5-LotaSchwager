23a09ca65cc196ede33cd222b828623aaf5e7525
LotaSchwager <>
2022-04-24 16:55:49 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -88,13 +88,15 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
     
     if (key > nodeKey->pair->key){
       if (nodeKey->right == NULL) return NULL;
-      nodeKey = nodeKey->left;
+      nodeKey = nodeKey->right;
     }
 
     if (key < nodeKey->pair->key){
       if (nodeKey->left == NULL) return NULL;
-      nodeKey = nodeKey->right;
+      nodeKey = nodeKey->left;
     }
+
+    if (nodeKey == NULL) return NULL;
   }
   return NULL;
 }
